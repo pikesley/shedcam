b5f30fdb48d87a7781f93ff9a7b1fafe081a2581
pikesley <github@orgraphone.org>
2017-12-28 22:17:56 +0000

    This should not go in git

--- spec/shedcam/shedcam_spec.rb
+++ spec/shedcam/shedcam_spec.rb
@@ -1,6 +1,6 @@
 module Shedcam
   describe App do
-    it 'says ShedCam' do
+    it 'says Shedcam' do
       get '/'
       expect(last_response).to be_ok
       expect(last_response.body).to match /Shedcam/
